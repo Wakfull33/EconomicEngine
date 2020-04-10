b5f9ae538eee0e126795537c9fba6d509aafcbe0
DarkBouffeur <farnham.tiihiva@gmail.com>
2020-04-10 04:51:03 -1000

    edit parser , try to do a template but not done :/
    add my pseudo code

--- include/Engine/Parser.h
+++ include/Engine/Parser.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "json.hpp"
 #include <string>
 #include <fstream>
 #include <vector>
@@ -6,24 +7,26 @@
 
 class Entity;
 class Simulation;
+template <typename  T>
 class Parser {
 
+	
 public:
 	//Constructor and Destructor
-	Parser();
-	Parser(std::string Path);
-	Parser(const Parser& ConstructParser);
-	~Parser();
+	Parser() = default;
+	Parser<T>::Parser(std::string Path);
+	Parser<T>::Parser(const Parser& ConstructParser);
+	Parser<T>:~Parser();
 
 	//Getter and Setter
-	std::string GetPath() const;
-	void SetPath(std::string& NewPath);
+	std::string ::Parser<T>::GetPath() const;
+	void Parser<T> ::SetPath(std::string& NewPath);
 	
 	//Operator
-	Parser& operator=(const Parser& CopyParser);
+	Parser& Parser<T>::operator=(const Parser& CopyParser);
 
-	void RegisterEntities() const;
-	void InitSimulationFromData(Simulation* EconomicSimulation);
+	void Parser<T>::RegisterEntities() const;
+	void Parser<T>::InitSimulationFromData(Simulation* EconomicSimulation);
 	
 
 private:
@@ -34,3 +37,20 @@ private:
 	std::ofstream Writer;
 };
 
+/*class en template
+ *get toutes les data du JsOn
+ *trier les gold de base  --> avoir la sommes des gold
+ *trier les metiers
+ *trier les items
+ *trier le nombres de cycles
+ *trier le temps de
+ *Envoyer le nombres d'agent en fonction du metier
+ *Envoyer le nombres d'item
+ *envoyer les gold par métiers
+ *(discuter sur tous les parametres de la simulations)
+ *
+ *
+ *
+ *
+ * 
+ */
