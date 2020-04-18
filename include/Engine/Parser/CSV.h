05422f3cc0dc09916e68deb6c952de801dec2e11
wakfull33 <thomas.baudou33@hotmail.com>
2020-04-18 03:06:04 +0200

    fix: procedurally generate csv columns

--- include/Engine/Parser/CSV.h
+++ include/Engine/Parser/CSV.h
@@ -13,14 +13,13 @@ public:
 	static bool IsFileValid(std::string FileName);
 
 	static void Write(std::vector<CycleResult>& Results, std::string DatasPath);
-	static void WriteProfit(std::vector<CycleResult>& Results, std::string DatasPath);
-	static void WriteMoney(std::vector<CycleResult>& Results, std::string DatasPath);
-	static void WriteItemProd(std::vector<CycleResult>& Results, std::string DatasPath);
-	static void WriteItemConsum(std::vector<CycleResult>& Results, std::string DatasPath);
-	static void WriteFood(std::vector<CycleResult>& Results, std::string DatasPath);
+	static void WriteProfit(std::vector<CycleResult>& Results, std::string DatasPath, std::string Columns);
+	static void WriteMoney(std::vector<CycleResult>& Results, std::string DatasPath, std::string Columns);
+	static void WriteItemProd(std::vector<CycleResult>& Results, std::string DatasPath, std::string Columns);
+	static void WriteItemConsum(std::vector<CycleResult>& Results, std::string DatasPath, std::string Columns);
+	static void WriteFood(std::vector<CycleResult>& Results, std::string DatasPath, std::string Columns);
 
-	static std::string BoolToString(bool value);
-
+	static std::string GetJobColumns();
 	
 private:
 
